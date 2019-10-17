d29712436199409fa7d7754307cbf11055e8ba56
Arnstein Kvande <mcs_works@hotmail.com>
2019-10-17 12:30:46 +0200

    Added debug

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,30 @@
 module.exports = {
-    "roots": [
-      "<rootDir>/src"
-    ],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-  }
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.json',
+      stringifyContentPathRegex: '\\.html$',
+    }
+  },
+  
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest'
+  },
+  testMatch: [
+    '**/__tests__/**/*.+(ts|js)?(x)',
+    '**/+(*.)+(spec|test).+(ts|js)?(x)'
+  ],
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@shared/(.*)': '<rootDir>/src/app/shared/$1'
+  },
+  transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!src/app/**/*.module.ts',
+    '!src/app/**/*.array.ts',
+    '!src/app/fragmentTypes.ts'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', 'src/app/*.{js}'],
+  
+};
