3fa6060a32ec56f8fc856b84ea8bb11b48c0fd0b
Arnstein <mcs_works@hotmail.com>
2020-05-14 22:15:38 +0200

    Added jest angular starter project

---
+++ angular/README.md
@@ -0,0 +1,5 @@
+# check this one out 
+https://medium.com/angular-in-depth/angular-cli-ng-test-with-jest-in-3-minutes-v2-1060ddd7908d
+
+run ng test --watch
+
---
+++ angular/src/app/app.component.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+
+  it('true should be true', () => {
+    expect(true).toBeTruthy();
+  });
+});
